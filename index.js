21fb3f8e5ec6b9a47073f8cc3dadc2c48a5702a1
ingxrd <ingridmunhoz86@gmail.com>
2024-07-30 18:01:34 -0300

    Alterações erro index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { connection, authenticate} from "./config/database.js";
 import {Cliente} from "./models/cliente.js";
-import {Cachorro} from "./models/endereco.js"
-import {Reserva} from "./models/pet.js"
+import {Cachorro} from "./models/cachorro.js"
+import {Reserva} from "./models/reserva.js"
 
 authenticate(connection).then(() => {
     connection.sync();
