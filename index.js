4cd87ffe0874bccacdea11572c2711cdad73cbf5
flaviare1s <flaviarster@gmail.com>
2024-07-30 17:58:28 -0300

    Correção de erro

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { connection, authenticate} from "./config/database.js";
 import {Cliente} from "./models/cliente.js";
-import {Cachorro} from "./models/endereco.js"
+import {Cachorro} from "./models/cachorro.js"
 import {Reserva} from "./models/pet.js"
 
 authenticate(connection).then(() => {
