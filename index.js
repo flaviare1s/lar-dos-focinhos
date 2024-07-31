8601d4b04a5f9e066c03000b605e9a70210d0dc1
flaviare1s <flaviarster@gmail.com>
2024-07-31 17:22:22 -0300

    Feita a conexão com express

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ import {Reserva} from "./models/reserva.js"
 authenticate(connection).then(() => {
     connection.sync();
 });
+
+// CRUD:
+// CREATE (POST):
+
+
+// READ (GET):
+
+
+// UPDATE (PUT):
+
+
+// DELETE (DELETE):
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000/')
+  })
+  
