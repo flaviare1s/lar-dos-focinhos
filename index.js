94cec03025ba8be899884156216da89da7afe3c1
flaviare1s <flaviarster@gmail.com>
2024-07-31 17:27:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 import { connection, authenticate} from "./config/database.js";
 import {Cliente} from "./models/cliente.js";
-import {Pet} from "./models/pet.js"
-import {Reserva} from "./models/reserva.js"
+import {Cachorro} from "./models/endereco.js"
+import {Reserva} from "./models/pet.js"
 
 authenticate(connection).then(() => {
     connection.sync();
 });
-
-const app = express();
-
-app.use(express.json());
-
-// CRUD:
-// CREATE (POST):
-
-
-// READ (GET):
-
-
-// UPDATE (PUT):
-
-
-// DELETE (DELETE):
-
-
-
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000/')
-  })
-  
