15ed8dd3c834507f1447c0ab775dbdaef570eaf8
flaviare1s <flaviarster@gmail.com>
2024-08-02 10:33:25 -0300

    Adicionado CRUD de pet

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import { connection, authenticate} from "./config/database.js";
 import express from 'express';
 import { clientesRouter } from "./routers/clientes.js";
 import { reservasRouter } from "./routers/reservas.js";
-//import { petsRouter } from "./routes/pets.js";
-
+import { petsRouter } from "./routers/pet.js";
 
 authenticate(connection).then(() => {
     connection.sync();
@@ -16,9 +15,7 @@ app.use(express.json());
 //Rotas 
 app.use(clientesRouter);
 app.use(reservasRouter);
-//app.use(petsRouter);
-
-
+app.use(petsRouter);
 
 app.listen(3001, () => {
     console.log('Servidor rodando em http://localhost:3001/')
